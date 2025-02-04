0b8a220128c6f920ecd4e77efb7ce8b0d9ff8e0e
Breandan Choi <cbreandan@gmail.com>
2025-02-04 14:13:33 +0800

    Update database.js

--- LibraryManagementAPI/database.js
+++ LibraryManagementAPI/database.js
@@ -1,3 +1,3 @@
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('postgres://test_user:12345@example.com:5432/dbname')
+const sequelize = new Sequelize('postgres://test_user:12345@localhost:5432/library')
 
